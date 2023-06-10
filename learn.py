847b26416c276d33d69f722bdde4e6c102f098aa
itsTofan <tofan.lie@gmail.com>
2023-06-10 16:20:56 +0930

    modify print function

--- learn.py
+++ learn.py
@@ -6,7 +6,7 @@ def check_reboot():
 
 def main():
     if check_reboot():
-        print ("Pending Reboot Computer")
+        print ("Pending Reboot")
         sys.exit(1)
     print ("Everything ok")
     sys.exit(0)
