3ee70cf6289448db0f8b6622de7011aab997dfde
itsTofan <106501521+itsTofan@users.noreply.github.com>
2023-06-10 16:32:15 +0930

    new print

--- learn.py
+++ learn.py
@@ -6,7 +6,7 @@ def check_reboot():
 
 def main():
     if check_reboot():
-        print ("Pending Reboot")
+        print ("Pending Reboot MY PC")
         sys.exit(1)
     print ("Everything ok")
     sys.exit(0)
