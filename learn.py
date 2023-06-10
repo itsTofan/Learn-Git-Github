f8d3158d266144e72e470dcb803b07936fc6bfb2
Tofan <tofan.lie@gmail.com>
2023-06-10 14:31:50 +0930

    new function call check reboot

--- learn.py
+++ learn.py
@@ -1,10 +1,12 @@
 import os
-
+import sys
 def check_reboot():
     """return true if has pending reboot"""
     return os.path.exists("/run/reboot-required")
 
 def main():
-    print("Hello World!")
+    if check_reboot():
+        print ("Pending Reboot")
+        sys.exit(1)
 
 main()
